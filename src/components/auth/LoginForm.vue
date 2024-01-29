aba6315d920a4ffbbb078778138059364af5ad62
Jhonatan Nobrega <jhonatan_nobrega@hotmail.com>
2024-01-29 20:10:30 -0300

    Criado store auth e atualizado o uso para ela no login

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,71 +1,70 @@
 <template>
-    <v-alert v-if="feedbackMessage" color="error" class="mb-2">{{ feedbackMessage }}</v-alert>
-    <v-form @submit.prevent="submit">
-        <v-row class="d-flex mb-3">
-            <v-col cols="12">
-                <v-label class="font-weight-bold mb-1">E-mail</v-label>
-                <v-text-field v-model="email" :error-messages="errors.email" variant="outlined" color="primary" />
-            </v-col>
-            <v-col cols="12">
-                <v-label class="font-weight-bold mb-1">Senha</v-label>
-                <v-text-field v-model="password" :error-messages="errors.password" variant="outlined" type="password"
-                    color="primary" />
-            </v-col>
-            <v-col cols="12" class="pt-0">
-                <div class="d-flex flex-wrap align-center ml-n2">
+  <v-alert v-if="feedbackMessage" color="error" class="mb-2">{{ feedbackMessage }}</v-alert>
+  <v-form @submit.prevent="submit">
+    <v-row class="d-flex mb-3">
+      <v-col cols="12">
+        <v-label class="font-weight-bold mb-1">E-mail</v-label>
+        <v-text-field v-model="email" :error-messages="errors.email" variant="outlined" color="primary" />
+      </v-col>
+      <v-col cols="12">
+        <v-label class="font-weight-bold mb-1">Senha</v-label>
+        <v-text-field v-model="password" :error-messages="errors.password" variant="outlined" type="password"
+          color="primary" />
+      </v-col>
+      <v-col cols="12" class="pt-0">
+        <div class="d-flex flex-wrap align-center ml-n2">
 
-                    <div class="ml-sm-auto">
-                        <RouterLink to="/"
-                            class="text-primary text-decoration-none text-body-1 opacity-1 font-weight-medium">
-                            Esqueceu sua senha?
-                        </RouterLink>
-                    </div>
-                </div>
-            </v-col>
-            <v-col cols="12" class="pt-0">
-                <v-btn type="submit" :loading="isSubmitting" color="primary" size="large" block flat>Entrar</v-btn>
-            </v-col>
-        </v-row>
-    </v-form>
+          <div class="ml-sm-auto">
+            <RouterLink to="/" class="text-primary text-decoration-none text-body-1 opacity-1 font-weight-medium">
+              Esqueceu sua senha?
+            </RouterLink>
+          </div>
+        </div>
+      </v-col>
+      <v-col cols="12" class="pt-0">
+        <v-btn type="submit" :loading="isSubmitting" color="primary" size="large" block flat>Entrar</v-btn>
+      </v-col>
+    </v-row>
+  </v-form>
 </template>
 
 <script setup>
-import axios from 'axios';
 import { ref } from 'vue';
 import { useForm, useField } from 'vee-validate';
 import { object, string } from 'yup';
 import { toTypedSchema } from '@vee-validate/yup';
 import { useRouter } from 'vue-router';
+import { useAuthStore } from '@/store/auth';
 
 const schema = toTypedSchema(object({
-    email: string().email().required().label('E-mail'),
-    password: string().required().label('Senha'),
+  email: string().email().required().label('E-mail'),
+  password: string().required().label('Senha'),
 }));
 const { handleSubmit, errors, isSubmitting } = useForm({ validationSchema: schema });
 
 const submit = handleSubmit(async (values) => {
-    await login(values);
+  await login(values);
 });
 
 const { value: email } = useField('email');
 const { value: password } = useField('password');
 
-// const email = ref("test@example.com");
-// const password = ref("password");
 const feedbackMessage = ref("");
+const authStore = useAuthStore();
 const router = useRouter();
+
 function login(values) {
-    feedbackMessage.value = "";
-    axios.get('sanctum/csrf-cookie')
+  feedbackMessage.value = "";
+  authStore
+    .sanctum()
+    .then(() => {
+      authStore
+        .login(values.email, values.password)
         .then(() => {
-            axios.post('api/login', {
-                email: values.email,
-                password: values.password
-            }).then(() => {
-                router.push({ name: 'dashboard' })
-            }).catch(() => {
-                feedbackMessage.value = 'Seu e-mail ou senha estão inválidos.'
-            });
+          router.push({ name: 'dashboard' })
+        }).catch(() => {
+          feedbackMessage.value = 'Seu e-mail ou senha estão inválidos.'
         });
+    });
 }
 </script>
